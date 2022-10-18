ee0c734addda9e3849a889cb24d4d39160e1f0e9
santomi3112 <adiprasetio007@gmail.com>
2022-10-18 20:04:36 +0700

    showed number to screen

--- script.js
+++ script.js
@@ -6,8 +6,48 @@ const allClearButton = document.querySelector("[data-all-clear]");
 const outputCurrentText = document.querySelector("[data-output-current]");
 const outputBeforeText = document.querySelector("[data-output-before]");
 
+function add(number, secondNumber) {
+  return number + secondNumber;
+}
+
+function substract(number, secondNumber) {
+  return number - secondNumber;
+}
+
+function multiply(number, secondNumber) {
+  return number * secondNumber;
+}
+
+function divide(number, secondNumber) {
+  return number / secondNumber;
+}
+
+function operate(operator, number, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(number, secondNumber);
+      break;
+    case "-":
+      return substract(number, secondNumber);
+      break;
+    case "x":
+      return multiply(number, secondNumber);
+      break;
+    case "÷":
+      if (b === 0) return "lmao";
+      return divide(number, secondNumber);
+      break;
+    default:
+      return;
+  }
+}
+
 numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    outputCurrentText.appendNumber(button.innerText);
-  });
+  button.addEventListener("click", () => appendNumber(button.textContent));
 });
+
+function appendNumber(number) {
+  outputCurrentText.textContent += number
+}
+
+function display(number) {}
